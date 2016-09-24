2e78ac016cb731ece445e25e34bc9f89ec4749cb
Wzedlare <vedatak01@gmail.com>
2016-09-24 15:27:14 +0300

    Initial commit for Android N

--- shamrock/proprietary/bin/install-recovery.sh
+++ shamrock/proprietary/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:32575488:e595178214ea182db196d24324cbfa959376a84d; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:31711232:c8ade5d8ec1dbec9586071c96e4b698051611b6f EMMC:/dev/block/bootdevice/by-name/recovery e595178214ea182db196d24324cbfa959376a84d 32575488 c8ade5d8ec1dbec9586071c96e4b698051611b6f:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/soc.0/7824900.sdhci/by-name/recovery:36145152:042b71b395a2d50da97a22c72fb8777ceb4fe9de; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/soc.0/7824900.sdhci/by-name/boot:32051200:e58bdc661704e287cdccccb0accc6208136b0d93 EMMC:/dev/block/platform/soc.0/7824900.sdhci/by-name/recovery 042b71b395a2d50da97a22c72fb8777ceb4fe9de 36145152 e58bdc661704e287cdccccb0accc6208136b0d93:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
