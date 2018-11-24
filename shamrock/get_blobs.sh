d21fdcea9ac99776d190f74e6ba75fbb77afd192
caneray <punisher_caner@windowslive.com>
2018-11-24 22:51:53 +0300

    Remove unneeded files

--- shamrock/get_blobs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-clear
-rm -rf proprietary-files.txt
-for f in $(find proprietary) ; do
-if [ ! -d "$f" ]; then
-x=`echo $f | sed "s/proprietary\///g"`
-  echo "    vendor/google/shamrock/$f:/system/$x \\" >> proprietary-files.txt
-fi
-done
